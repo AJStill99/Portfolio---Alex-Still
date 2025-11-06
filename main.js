ad04ef52579e7e253c9bfd5ce3aa853758484e09
Alex Still <alexstill@hotmail.co.uk>
2025-11-06 13:58:33 +0000

    Adding HTML, CSS and JS

---
+++ main.js
@@ -0,0 +1,21 @@
+/* BUTTONS
+    Functions that will be attached to the buttons
+*/
+
+function onButtonClick() {
+    addEventListener("click", handleClick);
+    console.log("Button was clicked!");
+}
+
+function handleClick() {
+    alert("Button Clicked!");
+}
+
+/* INPUT FIELDS
+    Functions that will be attached to input fields
+*/
+
+/* TEXT HANDLERS
+    Functions that will manipulate text content
+*/
+
