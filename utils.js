150e52e5217468a47e1d2004297c6ed359bb8f45
Alex Still <alexstill@hotmail.co.uk>
2025-11-12 23:31:20 +0000

    Added in helper functions

--- utils.js
+++ utils.js
@@ -17,9 +17,32 @@ async function checkHeaderText(expectedText) {
     expect(headerText).toBe(expectedText);
 }
 
+async function getElement(page, elementID) {
+    await page.locator(elementID);
+}
+
+async function loginPage(page, userEmailLocator, userEmail, userPasswordLocator, userPassword, loginButton, url) {
+    goToURL(url);
+    await page.locator(userEmailLocator).fill(userEmail);
+    await page.locator(userPasswordLocator).fill(userPassword);
+    await page.locator(loginButton).click(middle);
+}
+
+async function checkForSuccessElement(page, successElement, failureElement) {
+    const successLocator = page.locator(successElement);
+    const failureLocator = page.locator(failureElement);
+
+    if (await successLocator.isVisible()) {
+        await expect(successLocator).toBeVisible();
+    } else {
+        await expect(failureLocator).toBeVisible();
+    } 
+}
+
 module.exports = {
     goToURL,
     checkPageTitle,
     checkHeaderText,
-
+    getElement,
+    loginPage,
 };
